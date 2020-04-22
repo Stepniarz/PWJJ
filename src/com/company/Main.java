a6f77447d1b6112ed2a7e02a0060957623d41522
Stepniarz <98mati06@wp.pl>
2020-04-22 11:44:18 +0200

    Animal and Human in Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,18 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+	Animal dog = new Animal("dog");
+	dog.name = "Diego";
+	Animal lion = new Animal("lion");
+	lion.name = "Simba";
+	Animal mouse = new Animal("mouse");
+	mouse.name = "Mickey";
+	Human me = new Human();
+	me.firstName = "Mateusz";
+	me.lastName = "Stępiński";
+	me.pet = dog;
+
     }
 }
