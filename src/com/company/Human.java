3fb222aabd5b5ce71fe16d3b21f41d31d8449629
Stepniarz <98mati06@wp.pl>
2020-04-22 11:33:10 +0200

    QUICK SAVE

---
+++ src/com/company/Human.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Human {
+    String species;
+    String firstName;
+    String lastName;
+    Animal pet;
+    Phone phone;
+   /*{ public Human(String species)
+
+        this.species = species;
+        if(species == "Matt")
+    }*/
+}
